ae3476cb168fc2fec807fa8b7e8ece652bb09561
mattyb709 <matthew.builes8@gmail.com>
2026-03-16 20:59:23 -0500

    ts does not work

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate serialport;
+use serialport::TTYPort;
+use std::io::{Write, Read};
+
+fn main() {
+    let port_build = serialport::new("/dev/ttyACM0", 115200).timeout(std::time::Duration::from_millis(5000));
+    let mut tty_port = TTYPort::open(&port_build).expect("Failed to open port");
+    println!("Port opened successfully!");
+    let bytes: [u8; 6] = [0xff, 0xff, 0x01, 0x02, 0x01, 0xfb];
+    let bytes = tty_port.write(&bytes).expect("Failed to write to port");
+    tty_port.flush().expect("Failed to flush port");
+    println!("Bytes written: {}", bytes);
+
+    let mut buffer = [0; 64];
+    tty_port.read(&mut buffer).expect("Failed to read from port");
+    
+
+    println!("Bytes read: {:?}", buffer);
+
+}
